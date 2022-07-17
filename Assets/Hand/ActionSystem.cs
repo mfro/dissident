89c13fdce60f25643ec370ce3f457b4de6ea8d41
Scott <ssscrazy@live.com>
2022-07-16 20:00:09 -0400

    action scene

---
+++ Assets/Hand/ActionSystem.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionSystem : MonoBehaviour
+{
+
+    [SerializeField]
+    private int maxActions;
+    [SerializeField]
+    private int currentActions;
+
+    [SerializeField] private PixelText label; 
+
+    public void AdjustActions(int amount)
+    {
+        currentActions += amount;
+        UpdateUI();
+    }
+
+    public void RefreshAllActions()
+    {
+        AdjustActions(maxActions - currentActions);
+    }
+
+    private void UpdateUI()
+    {
+        label.text = currentActions.ToString() + " / " + maxActions.ToString();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RefreshAllActions();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
