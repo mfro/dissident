6823a8aba6b648ba67e29e56ed4909a5169b192d
Reid Shinabarker <reidshina6@gmail.com>
2022-07-17 02:02:34 -0400

    Walking Man
    Created Walking Man that has a walking animation and will move in a direction

---
+++ Assets/Walking Man/WalkingManController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkingManController : MonoBehaviour
+{
+    public float movementSpeed = 10f;
+    public Vector3 direction = new Vector3(0, 1, 0);
+    bool moving = false;
+
+    Animator _anim;
+
+    public bool toggleWalking = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (moving)
+        {
+            transform.position += direction * movementSpeed * Time.deltaTime;
+        }
+        if (toggleWalking)
+        {
+            ToggleWalking();
+            toggleWalking = false;
+        }
+    }
+
+    public void SetWalking(bool walking)
+    {
+        if (moving == walking)
+        {
+            return;
+        }
+        moving = walking;
+        _anim.SetBool("Walking", moving);
+    }
+
+    public void ToggleWalking()
+    {
+        SetWalking(!moving);
+    }
+}
